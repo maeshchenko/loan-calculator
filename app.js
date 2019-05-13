29fb058a781c7b6ea83347f280839ecb43a7716d
mikhail.eshchenko <mikhail.eshchenko@gmail.com>
2019-05-13 14:56:50 +0300

    add Loader and UI fixed

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  //Hide results
+  document.getElementById('results').style.display = 'none';
+
+  //Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 //Calculate results
-function calculateResults(e){
-  console.log('calculating...');
+function calculateResults(){
   //UI Vars
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -24,15 +33,27 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    //Show results
+    document.getElementById('results').style.display = 'block';
+    //Hide loader
+      document.getElementById('loading').style.display = 'none';
+    
   }else{
     showError('Ошибка. Проверьте введенные данные')
   }
 
-  e.preventDefault();
+  
+  
 };
 
 //showError
 function showError(error){
+  //Hide results
+  document.getElementById('results').style.display = 'none';
+  //Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   //create div
   const errorDiv = document.createElement('div');
 
